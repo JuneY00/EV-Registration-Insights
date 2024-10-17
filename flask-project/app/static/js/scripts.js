b57f2bba5d378d1e6e822d73c9f8a32e6e72c07d
jongmin1 <jm00512@naver.com>
2024-10-17 14:43:09 +0900

    feat: 사용하지 않는 요소 삭제 및 UI 변경

--- flask-project/app/static/js/scripts.js
+++ flask-project/app/static/js/scripts.js
@@ -7,18 +7,44 @@
 // Scripts
 // 
 
+// window.addEventListener('DOMContentLoaded', event => {
+
+//     // Toggle the side navigation
+//     const sidebarToggle = document.body.querySelector('#sidebarToggle');
+//     if (!sidebarToggle) {
+//         // Uncomment Below to persist sidebar toggle between refreshes
+//         // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+//         //     document.body.classList.toggle('sb-sidenav-toggled');
+//         // }
+//         sidebarToggle.addEventListener('click', event => {
+//             event.preventDefault();
+//             document.body.classList.toggle('sb-sidenav-toggled');
+//             localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+//         });
+//     }
+
+// });
+
 window.addEventListener('DOMContentLoaded', event => {
 
     // Toggle the side navigation
     const sidebarToggle = document.body.querySelector('#sidebarToggle');
     if (sidebarToggle) {
-        // Uncomment Below to persist sidebar toggle between refreshes
-        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
-        //     document.body.classList.toggle('sb-sidenav-toggled');
-        // }
+        // 로컬 스토리지에서 사이드바 상태 확인 및 초기화
+        const isSidebarToggled = localStorage.getItem('sb|sidebar-toggle');
+
+        // 로컬 스토리지 값이 없거나 'false'인 경우 사이드바를 닫힌 상태로 설정
+        if (isSidebarToggled === null || isSidebarToggled === 'false') {
+            document.body.classList.remove('sb-sidenav-toggled');
+        } else {
+            document.body.classList.add('sb-sidenav-toggled');
+        }
+
         sidebarToggle.addEventListener('click', event => {
             event.preventDefault();
             document.body.classList.toggle('sb-sidenav-toggled');
+
+            // 변경된 사이드바 상태를 로컬 스토리지에 저장
             localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
         });
     }
